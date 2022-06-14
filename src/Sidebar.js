898e72d041a06aaadb0a8856392f330b34696476
youngbin <redmangos@naver.com>
2022-06-14 21:35:54 +0900

    redux

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div ckassName="sidebar">
+      <h2>I am the sidebar</h2>
+    </div>
+  );
+}
+
+export default Sidebar;
