15cf0c457f24c1fb163074add5f2d40f6b5b795b
youngbin <redmangos@naver.com>
2022-06-15 22:16:59 +0900

    add icon

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     // BEM naming convention
     <div className="app">
-      <h1>Lets Build a DISCORD CLONE!!</h1>
       {/* {Sidebar} */}
       <Sidebar />
       {}
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Sidebar.css";
+import ExpandMoreSharpIcon from "@mui/icons-material/ExpandMoreSharp";
+import AddIcon from "@mui/icons-material/Add";
 
 function Sidebar() {
   return (
-    <div ckassName="sidebar">
-      <h2>I am the sidebar</h2>
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Clever Programmer</h3>
+        <ExpandMoreSharpIcon />
+      </div>
+
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreSharpIcon />
+            <h4>Text Channels</h4>
+          </div>
+
+          <AddIcon className="sidebar__addChannel" />
+        </div>
+      </div>
     </div>
   );
 }
