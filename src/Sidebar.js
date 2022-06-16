922527b5f48aa290b8ed63c0da9b64f9fb8283ac
youngbin <redmangos@naver.com>
2022-06-16 22:38:08 +0900

    sidebar #

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./Sidebar.css";
 import ExpandMoreSharpIcon from "@mui/icons-material/ExpandMoreSharp";
 import AddIcon from "@mui/icons-material/Add";
+import SidebarChannel from "./SidebarChannel";
+import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
 
 function Sidebar() {
   return (
@@ -19,6 +21,20 @@ function Sidebar() {
           </div>
 
           <AddIcon className="sidebar__addChannel" />
+        </div>
+        <div className="sidebar__channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon className="sidebar_voiceIcon" fontSize="large" />
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
         </div>
       </div>
     </div>
